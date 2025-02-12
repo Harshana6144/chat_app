b6e67e98c8a168a395e36b47fa4e215284965dd8
Harshana Prasad <harshana7096@gmail.com>
2025-02-12 22:18:48 +0530

    updated

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,10 @@
+import 'package:chat_app/components/my_button.dart';
 import 'package:chat_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
+
+//email and pw text controller
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _pwController = TextEditingController();
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -31,10 +36,25 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 25),
 
             //email textfield
-            MyTextField(),
-            //pw textfield
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
 
+            const SizedBox(height: 10),
+            //pw textfield
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+
+            const SizedBox(height: 25),
             //login button
+            MyButton(
+              text:"Login",
+            )
 
             //register now
           ],
