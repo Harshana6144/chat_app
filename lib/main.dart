ff89711ad5ecdc4fb5d126145ee3a0092e1ab725
Harshana Prasad <harshana7096@gmail.com>
2025-02-28 11:07:01 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import "package:chat_app/auth/Login_or_register.dart";
-import "package:chat_app/pages/login_page.dart";
-import "package:chat_app/pages/register_page.dart";
+import "package:chat_app/auth/auth_gate.dart";
+import "package:chat_app/firebase_options.dart";
 import "package:chat_app/themes/light_mode.dart";
+import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
       theme: lightMode,
     );
   }
