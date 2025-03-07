5c5bc1579f09f9e9d0184d33b1ea66f6e163b289
Harshana Prasad <harshana7096@gmail.com>
2025-03-07 12:39:30 +0530

    Updated

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,16 +1,61 @@
 import 'package:chat_app/components/my_drawer.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  //chat & auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  void logout() {
+    //get auth service
+    final auth = AuthService();
+    auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of user except for the curent logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUsersStream(),
+      builder: (context, snapshot) {
+        //error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading.");
+        }
+
+        //return  list view
+        return ListView(
+          children: snapshot.data!
+              .map<Widget>((userData) => _buildUserListItem(userData, context))
+              .toList(),
+        );
+      },
+    );
+  }
+
+//build individual list tile for user
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    //display all users expected current user
+    return UserTitle();
   }
 }
