bff5081c31a4d0693563b6ae48b8466cc4ac4975
Harshana Prasad <harshana7096@gmail.com>
2025-03-07 12:39:12 +0530

    Created user_title page

---
+++ lib/components/user_title.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserTitle extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  const UserTitle({
+    super.key,
+    required this.text,
+    required this.onTap,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
